24ec5bd7884a27e16f554bb4b19f025241b79ad8
EC2 Default User <ec2-user@ip-172-31-59-170.ec2.internal>
2020-02-26 03:43:42 +0000

    wip

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ which gcc || sudo yum groupinstall "Development Tools"
 ############
 # libevent #
 ############
-if [ ! -f $INSTALL_TO/dependencies/libevent ]; then
+if [ ! -d $INSTALL_TO/dependencies/libevent ]; then
 	wget https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}/libevent-${LIBEVENT_VERSION}.tar.gz
 	tar xvzf libevent-${LIBEVENT_VERSION}.tar.gz
 	cd libevent-${LIBEVENT_VERSION}
@@ -41,7 +41,7 @@ fi
 #############
 ## ncurses  #
 #############
-if [ ! -f $INSTALL_TO/dependencies/ncurses ]; then
+if [ ! -d $INSTALL_TO/dependencies/ncurses ]; then
 	wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz
 	tar xvzf ncurses-${NCURSES_VERSION}.tar.gz
 	cd ncurses-${NCURSES_VERSION}
@@ -58,21 +58,20 @@ libs="-L$INSTALL_TO/dependencies/libevent/lib -L$INSTALL_TO/dependencies/ncurses
 ############
 #   git    #
 ############
-if [ ! -f $INSTALL_TO/git ]; then
+if [ ! -d $INSTALL_TO/git ]; then
 	wget https://www.kernel.org/pub/software/scm/git/git-${GIT_VERSION}.tar.xz
 	tar -xvf git-${GIT_VERSION}.tar.xz
 	cd git-${GIT_VERSION}
 	./configure --prefix=$INSTALL_TO/git
 	make install
-	path_extra="$INSTALL_TO/git/bin:$path_extra"
 	cd ..
-	$INSTALL_TO/git/bin/git clone git@github.com:insperatum/me.git $INSTALL_TO/me
 fi
+path_extra="$INSTALL_TO/git/bin:$path_extra"
 
 ############
 #   tmux   #
 ############
-if [ ! -f $INSTALL_TO/tmux ]; then
+if [ ! -d $INSTALL_TO/tmux ]; then
 	wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
 	tar xvzf tmux-${TMUX_VERSION}.tar.gz
 	cd tmux-${TMUX_VERSION}
@@ -82,11 +81,12 @@ if [ ! -f $INSTALL_TO/tmux ]; then
 	  make install
 	cd ..
 fi
+path_extra="$INSTALL_TO/tmux/bin:$path_extra"
 
 ############
 #   vim    #
 ############
-if [ ! -f $INSTALL_TO/vim ]; then
+if [ ! -d $INSTALL_TO/vim ]; then
 	wget https://github.com/vim/vim/archive/v${VIM_VERSION}.tar.gz
 	tar -xvf v${VIM_VERSION}.tar.gz
 	cd vim-${VIM_VERSION}
@@ -95,11 +95,12 @@ if [ ! -f $INSTALL_TO/vim ]; then
 	make install
 	cd ..
 fi
+path_extra="$INSTALL_TO/vim/bin:$path_extra"
 
 ############
 #   zsh    #
 ############
-if [ ! -f $INSTALL_TO/vim ]; then
+if [ ! -d $INSTALL_TO/vim ]; then
 	wget https://sourceforge.net/projects/zsh/files/zsh/${ZSH_VERSION}/zsh-${ZSH_VERSION}.tar.xz/download -O zsh-${ZSH_VERSION}.tar.xz
 	tar -xvf zsh-${ZSH_VERSION}.tar.xz
 	cd zsh-${ZSH_VERSION}
@@ -109,21 +110,49 @@ if [ ! -f $INSTALL_TO/vim ]; then
 	  make install
 	cd ..
 fi
+path_extra="$INSTALL_TO/zsh/bin:$path_extra"
+
+
 
 # ------------- Extensions / Config -------------------
 # Oh-my-zsh
-PATH=$INSTALL_TO/zsh/bin:$PATH RUNZSH=no sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+if [ ! -d $HOME/.oh-my-zsh ]; then
+	PATH=$INSTALL_TO/zsh/bin:$PATH RUNZSH=no sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+fi
 
 # Zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+zshcustom=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+if [ ! -f ${zshcustom}/bullet-train.zsh-theme ]; then
+	wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -O ${zshcustom}/bullet-train.zsh-theme
+fi
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${zshcustom}/plugins/zsh-autosuggestions
+fi
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${zshcustom}/plugins/zsh-syntax-highlighting
+fi
 
 # tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d $HOME/.tmux/plugins/tpm ]; then
+	git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
 
 
-for dotfile in $(ls $INSTALL_TO/me/dotfiles); do
-	[ -f $HOME/$dotfile ] && mv $HOME/$dotfile $HOME/${dotfile}_$(date +"%F_%H.%M.%S")
+if [ ! -d $INSTALL_TO/me ]; then
+	$INSTALL_TO/git/bin/git clone git@github.com:insperatum/me.git $INSTALL_TO/me
+fi
+cd $INSTALL_TO/me
+git pull
+
+for dotfile in $(ls -a $INSTALL_TO/me/dotfiles | grep [^.]); do
+	echo Addding dotfile: $dotfile
+	if [ -f $HOME/$dotfile ]; then
+		if [ -L $HOME/$dotfile ]; then
+			rm $HOME/$dotfile	
+		else
+			mv $HOME/$dotfile $HOME/${dotfile}_$(date +"%F_%H.%M.%S")
+		fi
+	fi
 	ln -s $INSTALL_TO/me/dotfiles/$dotfile $HOME/$dotfile
 done
 
@@ -133,5 +162,8 @@ done
 # cleanup
 rm -rf $TEMP_DIR
 
-# for the in order to add to the .bashrc (for /sh/bash) comment-in below line
-# echo 'export PATH="$HOME/local/bin:$PATH"' >> $HOME/.bashrc
+# Add to bashrc #
+echo "export PATH=$path_extra:\$PATH" >> $HOME/.bashrc
+
+# set tmux shell
+echo "export DEFAULT_TMUX_SHELL=$INSTALL_TO/zsh/bin/zsh" >> $HOME/.bashrc
